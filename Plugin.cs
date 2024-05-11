4599284ae074a7f7a262fb430c35300ab11ec210
Wexop <mattheurtin@gmail.com>
2024-05-11 17:01:59 +0200

    v1.0.3

--- Plugin.cs
+++ Plugin.cs
@@ -2,26 +2,31 @@
 using HarmonyLib;
 using System.IO;
 using System.Reflection;
+using BepInEx.Configuration;
 using UnityEngine;
 using LethalLib.Modules;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
 
- namespace MoreLethalScraps
+namespace MoreLethalScraps
 {
     [BepInPlugin(GUID, NAME, VERSION)]
     [BepInDependency("evaisa.lethallib", "0.15.1")]
+    [BepInDependency("ainavt.lc.lethalconfig")]
     public class Plugin : BaseUnityPlugin
     {
 
         const string GUID = "wexop.moreLethalScraps";
         const string NAME = "MoreLethalScraps";
-        const string VERSION = "1.0.2";
+        const string VERSION = "1.0.3";
 
         public static Plugin instance;
 
         void Awake()
         {
             instance = this;
-            
+
             Logger.LogInfo($"MoreLethalScraps starting....");
 
             string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "morelethalscraps");
@@ -29,14 +34,22 @@ using LethalLib.Modules;
             
             Logger.LogInfo($"MoreLethalScraps bundle found !");
 
+            var scrapRarityDesc =
+                "The chance of finding an object, 0 is null 100 is a lot. Common game craps items are between 0 and 100 Warning: restart the game for the change to take effect.";
+
             //Dog
             Item dog = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/DogScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(dog.spawnPrefab);
             Utilities.FixMixerGroups(dog.spawnPrefab);
+            
+            var dogAllEntry = Config.Bind("ScrapsRarity", "DogToy All moons", 75, scrapRarityDesc);
+            CreateSliderScrap(dogAllEntry);
+            var dogSpecialMoonsEntry = Config.Bind("ScrapsRarity", "DogToy Assurance and Offense moons", 100, scrapRarityDesc);
+            CreateSliderScrap(dogSpecialMoonsEntry);
 
-            Items.RegisterScrap(dog, 100, Levels.LevelTypes.AssuranceLevel);
-            Items.RegisterScrap(dog, 100, Levels.LevelTypes.OffenseLevel);
-            Items.RegisterScrap(dog, 75, Levels.LevelTypes.All);
+            Items.RegisterScrap(dog, dogSpecialMoonsEntry.Value, Levels.LevelTypes.AssuranceLevel);
+            Items.RegisterScrap(dog, dogSpecialMoonsEntry.Value, Levels.LevelTypes.OffenseLevel);
+            Items.RegisterScrap(dog, dogAllEntry.Value, Levels.LevelTypes.All);
 
             Logger.LogInfo($"MoreLethalScraps - DogToy ready !");
             
@@ -44,10 +57,15 @@ using LethalLib.Modules;
             Item brakken = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/BrakkenScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(brakken.spawnPrefab);
             Utilities.FixMixerGroups(brakken.spawnPrefab);
+            
+            var brackenAllEntry = Config.Bind("ScrapsRarity", "BrackenToy All moons", 50, scrapRarityDesc);
+            CreateSliderScrap(brackenAllEntry);
+            var brackenSpecialMoonsEntry = Config.Bind("ScrapsRarity", "BrackenToy March and Vow moons", 80, scrapRarityDesc);
+            CreateSliderScrap(brackenSpecialMoonsEntry);
 
-            Items.RegisterScrap(brakken, 80, Levels.LevelTypes.MarchLevel);
-            Items.RegisterScrap(brakken, 80, Levels.LevelTypes.VowLevel);
-            Items.RegisterScrap(brakken, 50, Levels.LevelTypes.All);
+            Items.RegisterScrap(brakken, brackenSpecialMoonsEntry.Value, Levels.LevelTypes.MarchLevel);
+            Items.RegisterScrap(brakken, brackenSpecialMoonsEntry.Value, Levels.LevelTypes.VowLevel);
+            Items.RegisterScrap(brakken, brackenAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - BrakkenToy ready !");
             
@@ -55,7 +73,11 @@ using LethalLib.Modules;
             Item fireExit = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/FireExitScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(fireExit.spawnPrefab);
             Utilities.FixMixerGroups(fireExit.spawnPrefab);
-            Items.RegisterScrap(fireExit, 100, Levels.LevelTypes.All);
+            
+            var fireExitAllEntry = Config.Bind("ScrapsRarity", "FireExitToy All moons", 100, scrapRarityDesc);
+            CreateSliderScrap(fireExitAllEntry);
+            
+            Items.RegisterScrap(fireExit, fireExitAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - FireExitToy ready !");
             
@@ -63,7 +85,11 @@ using LethalLib.Modules;
             Item goldenFireExit = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/GoldenFireExitScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(goldenFireExit.spawnPrefab);
             Utilities.FixMixerGroups(goldenFireExit.spawnPrefab);
-            Items.RegisterScrap(goldenFireExit, 10, Levels.LevelTypes.All);
+            
+            var goldenFireExitAllEntry = Config.Bind("ScrapsRarity", "GoldenFireExitToy All moons", 5, scrapRarityDesc);
+            CreateSliderScrap(goldenFireExitAllEntry);
+            
+            Items.RegisterScrap(goldenFireExit, goldenFireExitAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - GoldenFireExitToy ready !");
             
@@ -71,11 +97,16 @@ using LethalLib.Modules;
             Item nutCracker = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/NutCrackerScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(nutCracker.spawnPrefab);
             Utilities.FixMixerGroups(nutCracker.spawnPrefab);
+            
+            var nutCrackerAllEntry = Config.Bind("ScrapsRarity", "NutCrackerToy All moons", 30, scrapRarityDesc);
+            CreateSliderScrap(nutCrackerAllEntry);
+            var nutCrackerSpecialMoonsEntry = Config.Bind("ScrapsRarity", "NutCrackerToy Rend, Dine and Titan moons", 75, scrapRarityDesc);
+            CreateSliderScrap(nutCrackerSpecialMoonsEntry);
 
-            Items.RegisterScrap(nutCracker, 75, Levels.LevelTypes.RendLevel);
-            Items.RegisterScrap(nutCracker, 75, Levels.LevelTypes.DineLevel);
-            Items.RegisterScrap(nutCracker, 75, Levels.LevelTypes.TitanLevel);
-            Items.RegisterScrap(nutCracker, 30, Levels.LevelTypes.All);
+            Items.RegisterScrap(nutCracker, nutCrackerSpecialMoonsEntry.Value, Levels.LevelTypes.RendLevel);
+            Items.RegisterScrap(nutCracker, nutCrackerSpecialMoonsEntry.Value, Levels.LevelTypes.DineLevel);
+            Items.RegisterScrap(nutCracker, nutCrackerSpecialMoonsEntry.Value, Levels.LevelTypes.TitanLevel);
+            Items.RegisterScrap(nutCracker, nutCrackerAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - NutCrackerToy ready !");
             
@@ -83,11 +114,16 @@ using LethalLib.Modules;
             Item giant = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/GiantScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(giant.spawnPrefab);
             Utilities.FixMixerGroups(giant.spawnPrefab);
+            
+            var giantAllEntry = Config.Bind("ScrapsRarity", "GiantToy All moons", 30, scrapRarityDesc);
+            CreateSliderScrap(giantAllEntry);
+            var giantSpecialMoonsEntry = Config.Bind("ScrapsRarity", "GiantToy March, Vow and Adamance moons", 80, scrapRarityDesc);
+            CreateSliderScrap(giantSpecialMoonsEntry);
 
-            Items.RegisterScrap(giant, 80, Levels.LevelTypes.VowLevel);
-            Items.RegisterScrap(giant, 80, Levels.LevelTypes.MarchLevel);
-            Items.RegisterScrap(giant, 80, Levels.LevelTypes.None, new []{"Adamance"});
-            Items.RegisterScrap(giant, 50, Levels.LevelTypes.All);
+            Items.RegisterScrap(giant, giantSpecialMoonsEntry.Value, Levels.LevelTypes.VowLevel);
+            Items.RegisterScrap(giant, giantSpecialMoonsEntry.Value, Levels.LevelTypes.MarchLevel);
+            Items.RegisterScrap(giant, giantSpecialMoonsEntry.Value, Levels.LevelTypes.None, new []{"Adamance"});
+            Items.RegisterScrap(giant, giantAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - GiantToy ready !");
             
@@ -95,12 +131,19 @@ using LethalLib.Modules;
             Item oldBird = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/OldBirdScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(oldBird.spawnPrefab);
             Utilities.FixMixerGroups(oldBird.spawnPrefab);
+            
+            var oldBirdAllEntry = Config.Bind("ScrapsRarity", "OldBirdToy All moons", 20, scrapRarityDesc);
+            CreateSliderScrap(oldBirdAllEntry);
+            var oldBirdSpecialMoonsEntry = Config.Bind("ScrapsRarity", "OldBirdToy Rend, Dine and Titan moons", 50, scrapRarityDesc);
+            CreateSliderScrap(oldBirdSpecialMoonsEntry);
+            var oldBirdEmbrionEntry = Config.Bind("ScrapsRarity", "OldBirdToy Embrion moon", 100, scrapRarityDesc);
+            CreateSliderScrap(oldBirdEmbrionEntry);
 
-            Items.RegisterScrap(oldBird, 50, Levels.LevelTypes.RendLevel);
-            Items.RegisterScrap(oldBird, 50, Levels.LevelTypes.DineLevel);
-            Items.RegisterScrap(oldBird, 65, Levels.LevelTypes.TitanLevel);
-            Items.RegisterScrap(oldBird, 100, Levels.LevelTypes.None, new []{"Embrion"});
-            Items.RegisterScrap(oldBird, 20, Levels.LevelTypes.All);
+            Items.RegisterScrap(oldBird, oldBirdSpecialMoonsEntry.Value, Levels.LevelTypes.RendLevel);
+            Items.RegisterScrap(oldBird, oldBirdSpecialMoonsEntry.Value, Levels.LevelTypes.DineLevel);
+            Items.RegisterScrap(oldBird, oldBirdSpecialMoonsEntry.Value, Levels.LevelTypes.TitanLevel);
+            Items.RegisterScrap(oldBird, oldBirdEmbrionEntry.Value, Levels.LevelTypes.None, new []{"Embrion"});
+            Items.RegisterScrap(oldBird, oldBirdAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - OldBirdToy ready !");
             
@@ -108,13 +151,27 @@ using LethalLib.Modules;
             Item blob = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/MoreLethalScraps/BlobScrap.asset");
             NetworkPrefabs.RegisterNetworkPrefab(blob.spawnPrefab);
             Utilities.FixMixerGroups(blob.spawnPrefab);
-            Items.RegisterScrap(blob, 40, Levels.LevelTypes.All);
+            
+            var blobAllEntry = Config.Bind("ScrapsRarity", "BlobToy All moons", 40, scrapRarityDesc);
+            CreateSliderScrap(blobAllEntry);
+            
+            Items.RegisterScrap(blob, blobAllEntry.Value, Levels.LevelTypes.All);
             
             Logger.LogInfo($"MoreLethalScraps - BlobToy ready !");
 
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
             Logger.LogInfo($"MoreLethalScraps is patched!");
+        }
+
+        private void CreateSliderScrap( ConfigEntry<int> configEntry)
+        {
+            var exampleSlider = new IntSliderConfigItem(configEntry, new IntSliderOptions 
+            {
+                Min = 0,
+                Max = 100
+            });
+            LethalConfigManager.AddConfigItem(exampleSlider);
         }
 
 
--- ThunderStore/README.md
+++ ThunderStore/README.md
@@ -1,1 +1,14 @@
 Add common lethal company monsters or anything in the game as scraps items (Be carefulf it can be noisy!)
+
+Every spawn rate can be configured with LethalConfig !
+
+Current item in v1.0.3:
+
+- DogToy
+- BrackenToy
+- NutCrackerToy
+- GiantToy
+- OldBirdToy
+- BlobToy
+- FireExitToy
+- GoldenFireExitToy
